2de8c8428fd1282ebad4146940dbffcc9bbc9759
Aravind419 <aravindaravind19720@gmail.com>
2026-08-13 15:47:07 +0530

    Vehicle base class

--- models/vehicle.py
+++ models/vehicle.py
@@ -4,16 +4,3 @@ class Vehicle:
         self.brand = brand
         self.rental_price = rental_price
         
-    def display_details(self):
-        return f"Vehicle brand is {self.brand} and vehicle number is {self.vehicle_number} rent per day is {self.rental_price}"
-    
-    def calculate_rental(self, days):
-        return self.rental_price * days
-    
-rental_day = int(input("How many days need vehicle for rent: "))
-
-car = Vehicle("123XXyz","BMW",1000)
-vehicle_info = car.display_details()
-rent = car.calculate_rental(rental_day)
-print(vehicle_info)
-print(f"rent for {rental_day} is {rent}")
